3e0cea764ea8cd5908a9d32d6852248d9168e02f
f-ibrahim <fadyh.shenouda@gmail.com>
2025-12-15 22:35:03 +0100

    - implemented configurator view to show regulations and their details on selection - removed dummy mainService as it belonged to another project

--- capgeminiappws2025/webapp/controller/Configurator.controller.js
+++ capgeminiappws2025/webapp/controller/Configurator.controller.js
@@ -7,37 +7,39 @@ sap.ui.define([
     return Controller.extend("capgeminiappws2025.controller.Configurator", {
 
         onInit: function () {
-      // Initialization code if needed
-    },
-
-    onAfterRendering: function () {
-      let oModel = this.getView().getModel();
-
-      oModel.read("/Z_I_ZCONFIG", {
-        success: function (oData) {
-          console.log("Config data:", oData);
+            // Initial setup if needed
         },
-        error: function (oError) {
-          console.error("Error fetching config data:", oError);
+
+        onSelectRegulation: function (oEvent) {
+            var oSelectedItem = oEvent.getParameter("listItem");
+            var oContext = oSelectedItem.getBindingContext(); // default model
+            var sPath = oContext.getPath();
+            console.log("Selected Regulation Path:", sPath);
+
+            var oRulesTable = this.byId("rulesTable");
+            console.log("Rules Table:", oRulesTable);
+
+            oRulesTable.bindItems({ 
+                path: sPath + "/to_Fields",
+                template: oRulesTable.getItems()[0].clone()  // Clone first item as template
+             });
+        },
+        
+        onPressAddRegulation: function (oEvent) {
+            // Logic to add a new regulation
+        },
+
+        onPressAddRule: function (oEvent) {
+            // Logic to add a new rule
+        },
+
+        onPressSaveConfiguration: function (oEvent) {
+            // Logic to save a configuraytion
+        },
+
+        onPressTestResults: function (oEvent) {
+            // Logic to test results
         }
-      });
-    },
-
-    onPressAddRegulation: function (oEvent) {
-        // Logic to add a new regulation
-    },
-
-    onPressAddRule: function (oEvent) {
-        // Logic to add a new rule
-    },
-
-    onPressSaveConfiguration: function (oEvent) {
-        // Logic to add a new rule
-    },
-
-    onPressTestResults: function (oEvent) {
-        // Logic to add a new rule
-    }
 
     });
 });
